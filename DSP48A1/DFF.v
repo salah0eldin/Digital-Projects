c3eae5d457747c755f437ad07c2fd27183f61ac0
Salah Eldin Hassen <67404369+salah0eldin@users.noreply.github.com>
2025-02-23 12:09:57 +0200

    Update DFF.v
    Added REGEN parameter

--- DSP48A1/DFF.v
+++ DSP48A1/DFF.v
@@ -1,7 +1,8 @@
 module DFF
     #(
          parameter WIDTH = 1,
-         parameter RSTTYPE = "ASYNC"
+         parameter RSTTYPE = "ASYNC",
+         parameter REGEN = 1 // 1 for flip-flop operation, 0 for direct assign
      )
      (
          input wire clk,
@@ -12,21 +13,27 @@ module DFF
      );
 
     generate
-        if (RSTTYPE == "ASYNC") begin
-            always @(posedge clk or posedge rst) begin
-                if (rst) begin
-                    q <= 0;
-                end else if (en) begin
-                    q <= d;
+        if (REGEN == 1) begin
+            if (RSTTYPE == "ASYNC") begin
+                always @(posedge clk or posedge rst) begin
+                    if (rst) begin
+                        q <= 0;
+                    end else if (en) begin
+                        q <= d;
+                    end
+                end
+            end else begin
+                always @(posedge clk) begin
+                    if (rst) begin
+                        q <= 0;
+                    end else if (en) begin
+                        q <= d;
+                    end
                 end
             end
         end else begin
-            always @(posedge clk) begin
-                if (rst) begin
-                    q <= 0;
-                end else if (en) begin
-                    q <= d;
-                end
+            always @(*) begin
+                q = d;
             end
         end
     endgenerate
